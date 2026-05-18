380cdff2cfa69bc916fadf76b94b2421684233d4
kleinlennart <31488915+kleinlennart@users.noreply.github.com>
2026-05-18 20:03:42 +0200

    sop

--- src/your_project_name/data.py
+++ src/your_project_name/data.py
@@ -2,5 +2,8 @@ from pathlib import Path
 import pandas as pd
 
 INPUT_PATH = Path("data", "raw", "input.csv")
+OUTPUT_PATH = Path("data", "raw", "output.parquet")
 
 df = pd.read_csv(INPUT_PATH)
+
+df.to_parquet(OUTPUT_PATH)
